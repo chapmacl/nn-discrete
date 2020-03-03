5db9aaa717a2fe2d5296fc5de08ed3e75682d3ac
thiagobell <thibef@gmail.com>
2020-03-03 23:21:25 +0100

    added method to generate discrete MnistReal model out of MnistTernaryTanh

--- discrete_nn/models/mnist/ternary_tanh_conv.py
+++ discrete_nn/models/mnist/ternary_tanh_conv.py
@@ -17,6 +17,8 @@ from discrete_nn.layers.logit_linear import TernaryLinear
 from discrete_nn.layers.conv import LogitConv
 from discrete_nn.layers.local_reparametrization import LocalReparametrization
 from discrete_nn.models.mnist.real_conv import MnistReal
+from discrete_nn.models.base_model import BaseModel
+
 from discrete_nn.layers.Flatten import Flatten
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -24,7 +26,7 @@ if device == "cuda:0":
     torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 
-class MnistTernaryTanh(torch.nn.Module):
+class MnistTernaryTanh(BaseModel):
     """
     Real valued (non convolutionary) network for the mnist dataset
     """
@@ -75,6 +77,64 @@ class MnistTernaryTanh(torch.nn.Module):
     def forward(self, x):
         # takes image vector
         return self.netlayers(x)
+
+    def get_net_parameters(self):
+        return self.state_dict()
+
+    def set_net_parameters(self, param_dict):
+        for k, v in param_dict.items():
+            self.state_dict()[k][:] = v
+
+    def generate_discrete_networks(self, method: str) -> MnistReal:
+        """
+
+        :param method: sample or argmax
+        :return:
+        """
+
+        """
+        repr_dict = dict()
+        repr_dict["L1_Conv_W"] = internal_dict["netlayers.0.weight"]
+        repr_dict["L1_BatchNorm_W"] = internal_dict["netlayers.2.weight"]
+        repr_dict["L1_BatchNorm_b"] = internal_dict["netlayers.2.bias"]
+        repr_dict["L2_Conv_W"] = internal_dict["netlayers.5.weight"]
+        repr_dict["L2_BatchNorm_W"] = internal_dict["netlayers.8.weight"]
+        repr_dict["L2_BatchNorm_b"] = internal_dict["netlayers.8.bias"]
+        repr_dict["L3_Linear_W"] = internal_dict["netlayers.11.weight"]
+        repr_dict["L3_Linear_b"] = internal_dict["netlayers.11.bias"].reshape(-1, 1)
+        repr_dict["L3_BatchNorm_W"] = internal_dict["netlayers.15.weight"]
+        repr_dict["L3_BatchNorm_b"] = internal_dict["netlayers.15.bias"]
+        repr_dict["L4_Linear_W"] = internal_dict["netlayers.14.weight"]
+        repr_dict["L4_Linear_b"] = internal_dict["netlayers.14.bias"].reshape(-1, 1)"""
+
+
+        # state dicts
+        l1_layer: LogitConv = self.netlayers[0]
+        l1_sampled_w, l1_sampled_b = l1_layer.generate_discrete_network(method)
+        l2_layer: LogitConv = self.netlayers[6]
+        l2_sampled_w, l2_sampled_b = l2_layer.generate_discrete_network(method)
+        l3_layer: TernaryLinear = self.netlayers[13]
+        l3_sampled_w, l3_sampled_b = l3_layer.generate_discrete_network(method)
+        l4_layer: TernaryLinear = self.netlayers[17]
+        l4_sampled_w, l4_sampled_b = l4_layer.generate_discrete_network(method)
+        state_dict = {
+            "L1_Conv_W": l1_sampled_w,
+            "L1_BatchNorm_W": self.state_dict()['netlayers.2.weight'],
+            "L1_BatchNorm_b": self.state_dict()['netlayers.2.bias'],
+            "L2_Conv_W": l2_sampled_w,
+            "L2_BatchNorm_W": self.state_dict()['netlayers.8.weight'],
+            "L2_BatchNorm_b": self.state_dict()['netlayers.8.bias'],
+            "L3_Linear_W": l3_sampled_w,
+            "L3_Linear_b": l3_sampled_b,
+            "L3_BatchNorm_W": self.state_dict()["netlayers.15.weight"],
+            "L3_BatchNorm_b": self.state_dict()["netlayers.15.bias"],
+            "L4_Linear_W": l4_sampled_w,
+            "L4_Linear_b": l4_sampled_b
+        }
+        real_net = MnistReal()
+        real_net.to(device)
+        real_net.set_net_parameters(state_dict)
+        return real_net
 
 
 class DatasetMNIST(Dataset):
