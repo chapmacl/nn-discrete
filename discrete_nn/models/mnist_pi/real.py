e3682b6a8378425329a3c9ba47dd3a8f61caf915
thiagobell <thibef@gmail.com>
2020-03-22 13:53:54 +0100

    refactored pi real experiment for mnist

--- discrete_nn/models/mnist_pi/real.py
+++ discrete_nn/models/mnist_pi/real.py
@@ -1,77 +1,8 @@
-"""
-This module implements the real valued (non convolutionary) network for the mnist dataset
-"""
 import torch
 from torch.utils.data import DataLoader
 
 from discrete_nn.dataset.mnist import MNIST
-from discrete_nn.models.base_model import BaseModel
-
-
-class MnistPiReal(BaseModel):
-    """
-    Real valued (non convolutionary) network for the mnist dataset
-    """
-
-    def __init__(self, weights=None):
-        """
-
-        :param weights: if not none contains the weighs for the networks layers
-        """
-        super().__init__()
-        # defining all the network's layers
-        self.netlayers = torch.nn.Sequential(
-
-            torch.nn.Dropout(p=0.2),
-            torch.nn.Linear(784, 1200),
-            torch.nn.BatchNorm1d(1200, momentum=0.1),
-            # momentum equivalent to alpha on reference impl.
-            # should batch normalization be here or after the activation function ?
-            torch.nn.Tanh(),
-            #
-            torch.nn.Dropout(p=0.4),
-            torch.nn.Linear(1200, 1200),
-            torch.nn.BatchNorm1d(1200, momentum=0.1),
-            torch.nn.Tanh(),
-            #
-            torch.nn.Dropout(p=0.4),
-            torch.nn.Linear(1200, 10))  # last layer outputs the unnormalized loglikelihood used by the softmax later
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=1e-3, weight_decay=1e-4)
-        self.loss_funct = torch.nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        # takes image vector
-        x = x
-        return self.netlayers(x)
-
-    def set_net_parameters(self, param_dict):
-        self.state_dict()["netlayers.1.weight"][:] = param_dict["L1_Linear_W"]
-        self.state_dict()["netlayers.1.bias"][:] = param_dict["L1_Linear_b"].reshape(-1)
-        self.state_dict()['netlayers.2.weight'][:] = param_dict["L1_BatchNorm_W"]
-        self.state_dict()['netlayers.2.bias'][:] = param_dict["L1_BatchNorm_b"]
-        self.state_dict()["netlayers.5.weight"][:] = param_dict["L2_Linear_W"]
-        self.state_dict()["netlayers.5.bias"][:] = param_dict["L2_Linear_b"].reshape(-1)
-        self.state_dict()['netlayers.6.weight'][:] = param_dict["L2_BatchNorm_W"]
-        self.state_dict()['netlayers.6.bias'][:] = param_dict["L2_BatchNorm_b"]
-        self.state_dict()["netlayers.9.weight"][:] = param_dict["L3_Linear_W"]
-        self.state_dict()["netlayers.9.bias"][:] = param_dict["L3_Linear_b"].reshape(-1)
-        return
-
-    def get_net_parameters(self):
-        """:returns a dictionary with the trainable parameters"""
-        internal_dict = {name: value for name, value in self.named_parameters()}
-        repr_dict = dict()
-        repr_dict["L1_Linear_W"] = internal_dict["netlayers.1.weight"]
-        repr_dict["L1_Linear_b"] = internal_dict["netlayers.1.bias"].reshape(-1, 1)
-        repr_dict["L1_BatchNorm_W"] = internal_dict["netlayers.2.weight"]
-        repr_dict["L1_BatchNorm_b"] = internal_dict["netlayers.2.bias"]
-        repr_dict["L2_Linear_W"] = internal_dict["netlayers.5.weight"]
-        repr_dict["L2_Linear_b"] = internal_dict["netlayers.5.bias"].reshape(-1, 1)
-        repr_dict["L2_BatchNorm_W"] = internal_dict["netlayers.6.weight"]
-        repr_dict["L2_BatchNorm_b"] = internal_dict["netlayers.6.bias"]
-        repr_dict["L3_Linear_W"] = internal_dict["netlayers.9.weight"]
-        repr_dict["L3_Linear_b"] = internal_dict["netlayers.9.bias"].reshape(-1, 1)
-        return repr_dict
+from discrete_nn.models.pi.real import PiReal
 
 
 def train_model():
@@ -88,7 +19,7 @@ def train_model():
     test_loader = DataLoader(dataset=mnist.test, batch_size=batch_size,
                              shuffle=False)
 
-    net = MnistPiReal()
+    net = PiReal()
     net = net.to(device)
 
     num_epochs = 100
