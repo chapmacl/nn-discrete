261cf37c0f43b5bdcb10107c98f7d184d08f8bdd
thiagobell <thibef@gmail.com>
2020-03-24 21:14:18 +0100

    added experiments with mnist_pi_nobias models

---
+++ discrete_nn/experiments/mnist_pi_no_bias/real.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import DataLoader
+
+from discrete_nn.dataset.mnist import MNIST
+from discrete_nn.models.pi_no_bias.real import PiNoBiasReal
+
+
+def train_model():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # basic dataset holder
+    mnist = MNIST(device, "flat")
+    # creates the dataloader for pytorch
+    batch_size = 100
+    train_loader = DataLoader(dataset=mnist.train, batch_size=batch_size,
+                              shuffle=True)
+    validation_loader = DataLoader(dataset=mnist.validation, batch_size=batch_size,
+                                   shuffle=False)
+    test_loader = DataLoader(dataset=mnist.test, batch_size=batch_size,
+                             shuffle=False)
+
+    net = PiNoBiasReal()
+    net = net.to(device)
+
+    num_epochs = 100
+    # will save metrics and model to disk
+    return net.train_model(train_loader, validation_loader, test_loader, num_epochs, model_name="MNIST-pi-NoBias-real")
+
+
+if __name__ == "__main__":
+    train_model()
---
+++ discrete_nn/experiments/mnist_pi_no_bias/ternary_sign.py
@@ -0,0 +1,42 @@
+"""
+This module implements the real valued (non convolutionary) network for the mnist dataset
+"""
+import os
+
+import torch
+from torch.utils.data import DataLoader
+
+from discrete_nn.dataset.mnist import MNIST
+from discrete_nn.settings import model_path
+from discrete_nn.layers.type_defs import DiscreteWeights
+from discrete_nn.models.pi_no_bias.logit_sign import PiNoBiasLogitSign
+
+
+def train_model(real_model_folder):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    batch_size = 100
+    # basic dataset holder
+    mnist = MNIST(device, "flat")
+    # creates the dataloader for pytorch
+    train_loader = DataLoader(dataset=mnist.train, batch_size=batch_size,
+                              shuffle=True)
+    validation_loader = DataLoader(dataset=mnist.validation, batch_size=batch_size,
+                                   shuffle=False)
+    test_loader = DataLoader(dataset=mnist.test, batch_size=batch_size,
+                             shuffle=False)
+
+    real_model_param_path = os.path.join(model_path, real_model_folder,
+                                         "PiReal.param.pickle")
+
+    real_param = torch.load(real_model_param_path, map_location="cpu")
+    logit_net = PiNoBiasLogitSign(real_param, DiscreteWeights.TERNARY)
+    logit_net = logit_net.to(device)
+
+    # evaluate first logit model before training, train and evaluate again
+
+    logit_net.train_model(train_loader, validation_loader, test_loader, 200, "MNIST-Pi-NoBias-Sign-Ternary", True)
+
+
+if __name__ == "__main__":
+    train_model("MNIST-pi-real-trained-2020-3-8--h17m34")
