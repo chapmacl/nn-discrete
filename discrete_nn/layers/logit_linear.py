36e2db5b04a6f271587371f016112e5340e9258f
thiagobell <thibef@gmail.com>
2020-03-24 21:10:56 +0100

    refactoring logit layer to support linear layers with no bias

--- discrete_nn/layers/logit_linear.py
+++ discrete_nn/layers/logit_linear.py
@@ -1,6 +1,8 @@
 """
 Implements different linear layer classes by extending from torch.nn.Module
 """
+from typing import Optional
+
 from discrete_nn.layers.type_defs import ValueTypes, DiscreteWeights
 from discrete_nn.layers.weight_utils import discretize_weights_probabilistic, generate_weight_probabilities, get_gaussian_dist_parameters
 
@@ -11,7 +13,8 @@ import torch
 
 class LogitLinear(nn.Module):
     def __init__(self, input_features, input_feature_type: ValueTypes, output_features, initialization_weights,
-                 initialization_bias, discretized_values: DiscreteWeights, normalize_activations: bool = False):
+                 initialization_bias: Optional[torch.Tensor], discretized_values: DiscreteWeights,
+                 normalize_activations: bool = False):
         """
         Initializes a linear layer with discrete weights
         :param input_features: the input dimension
@@ -20,7 +23,7 @@ class LogitLinear(nn.Module):
         :param initialization_weights: a tensor (output_features x input_features) with pre trained real weights to b/e
         used for initializing the discrete ones
         :param initialization_bias: a tensor (output_features x 1) with pre trained real weights for bias to be
-        used for initializing the discrete ones
+        used for initializing the discrete ones. If None, disables bias
         :param discretized_values: a list of the discrete weight values (e.g. [-1, 1, 0])
         :param normalize_activations: if true normalize the activations by dividing by the number of the layer's inputs
         (including bias)
@@ -35,8 +38,11 @@ class LogitLinear(nn.Module):
         self.W_logits = torch.nn.Parameter(discretize_weights_probabilistic(initialization_weights,
                                                                             self.discrete_values),
                                            requires_grad=True)
-        self.b_logits = torch.nn.Parameter(discretize_weights_probabilistic(initialization_bias, self.discrete_values),
-                                           requires_grad=True)
+        if initialization_bias is not None:
+            self.b_logits = torch.nn.Parameter(discretize_weights_probabilistic(initialization_bias, self.discrete_values),
+                                               requires_grad=True)
+        else:
+            self.b_logits = None
 
     def generate_discrete_network(self, method: str = "sample"):
         """ generates discrete weights from the weights of the layer based on the weight distributions
@@ -44,44 +50,56 @@ class LogitLinear(nn.Module):
         :param method: the method to use to generate the discrete weights. Either argmax or sample
 
         :returs: tuple (sampled_w, sampled_b) where sampled_w and sampled_b are tensors of the shapes
-        (output_features x input_features) and (output_features x 1)
+        (output_features x input_features) and (output_features x 1). sampled_b is None if bias is disable
         """
         probabilities_w = generate_weight_probabilities(self.W_logits).cpu()
-        probabilities_b = generate_weight_probabilities(self.b_logits).cpu()
-        assert(probabilities_b.shape != probabilities_w.shape)
+
+        if self.b_logits is not None:
+            probabilities_b = generate_weight_probabilities(self.b_logits).cpu()
+            assert(probabilities_b.shape != probabilities_w.shape)
+            probabilities_b = probabilities_b.transpose(0, 1).transpose(1, 2)
+        else:
+            probabilities_b = None
+
         # logit probabilities must be in inner dimension for torch.distribution.Multinomial
-        probabilities_w = probabilities_w.transpose(0, 1).transpose(1, 2)
         # stepped transpose bc we need to keep the order of the other dimensions
-        probabilities_b = probabilities_b.transpose(0, 1).transpose(1, 2)
+        probabilities_w = probabilities_w.transpose(0, 1).transpose(1, 2)
+
         discrete_values_tensor = torch.tensor(self.discrete_values).double()
 
         if method == "sample":
             m_w = Multinomial(probs=probabilities_w)
-            m_b = Multinomial(probs=probabilities_b)
+            if probabilities_b is not None:
+                m_b = Multinomial(probs=probabilities_b)
+                sampled_b = m_b.sample()
+                if torch.all(sampled_b.sum(dim=2) != 1):
+                    raise ValueError("sampled mask for bias does not sum to 1")
+                sampled_b = torch.matmul(sampled_b, discrete_values_tensor)
+            else:
+                sampled_b = None
+
             # this is a output_features x input_features x discretization_levels mask
             sampled_w = m_w.sample()
-            sampled_b = m_b.sample()
             # need to make sure these tensors are in the cpu
-
-            if torch.all(sampled_b.sum(dim=2) != 1):
-                raise ValueError("sampled mask for bias does not sum to 1")
             if torch.all(sampled_w.sum(dim=2) != 1):
                 raise ValueError("sampled mask for weights does not sum to 1")
 
             # need to generate the discrete weights from the masks
-
             sampled_w = torch.matmul(sampled_w, discrete_values_tensor)
-            sampled_b = torch.matmul(sampled_b, discrete_values_tensor)
+
         elif method == "argmax":
             # returns a (out_feat x in_feat) matrix where the values correspond to the index of the discretized value
             # with the largest probability
             argmax_w = torch.argmax(probabilities_w, dim=2)
-            argmax_b = torch.argmax(probabilities_b, dim=2)
+            if probabilities_b is not None:
+                argmax_b = torch.argmax(probabilities_b, dim=2)
+                sampled_b = torch.zeros_like(argmax_b).to("cpu")
+                sampled_b[:] = discrete_values_tensor[argmax_b[:]]
+            else:
+                sampled_b = None
             # creating placeholder for discrete weights
             sampled_w = torch.zeros_like(argmax_w).to("cpu")
-            sampled_b = torch.zeros_like(argmax_b).to("cpu")
             sampled_w[:] = discrete_values_tensor[argmax_w[:]]
-            sampled_b[:] = discrete_values_tensor[argmax_b[:]]
 
         else:
             raise ValueError(f"Invalid method {method} for layer discretization")
@@ -89,7 +107,7 @@ class LogitLinear(nn.Module):
         # sanity checks
         if sampled_w.shape != (probabilities_w.shape[0], probabilities_w.shape[1]):
             raise ValueError("sampled probability mask for weights does not match expected shape")
-        if sampled_b.shape != (probabilities_b.shape[0], probabilities_b.shape[1]):
+        if sampled_b is not None and sampled_b.shape != (probabilities_b.shape[0], probabilities_b.shape[1]):
             raise ValueError("sampled probability mask for bias does not match expected shape")
 
         return sampled_w, sampled_b
@@ -104,7 +122,6 @@ class LogitLinear(nn.Module):
         the mean and variance,  n is the number of samples and, out_feat is the number of out features.
         """
         w_mean, w_var = get_gaussian_dist_parameters(self.W_logits, self.discrete_values)
-        b_mean, b_var = get_gaussian_dist_parameters(self.b_logits, self.discrete_values)
 
         if self.in_feat_type == ValueTypes.REAL:
             # transpose the input matrix to (in_feat x n)
@@ -128,8 +145,10 @@ class LogitLinear(nn.Module):
         output_mean = output_mean.transpose(0, 1)
         output_var = output_var.transpose(0, 1)
 
-        output_mean += b_mean[:, 0]  # broadcasting to all samples
-        output_var += b_var[:, 0]
+        if self.b_logits is not None:
+            b_mean, b_var = get_gaussian_dist_parameters(self.b_logits, self.discrete_values)
+            output_mean += b_mean[:, 0]  # broadcasting to all samples
+            output_var += b_var[:, 0]
 
         if self.normalize_activations:
             output_mean /= (self.in_feat + 1) ** 0.5
