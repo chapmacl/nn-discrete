5c00813ff8ddedb9a961018b5b6f3cefe2730713
thiagobell <thibef@gmail.com>
2020-04-08 23:49:12 +0200

    fixed checkpointing issue caused by a submethod in the fashion mnist module

--- discrete_nn/dataset/fashion.py
+++ discrete_nn/dataset/fashion.py
@@ -1,10 +1,13 @@
 from typing import Tuple
+import os
+from functools import partial
 
 import torch
 from torch.utils.data import DataLoader
 from torchvision.datasets import FashionMNIST
 from torchvision.transforms import ToTensor
 
+from discrete_nn.settings import dataset_path
 
 def fashion_mnist_dataloaders(batch_size, device, feature_format) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """
@@ -14,26 +17,15 @@ def fashion_mnist_dataloaders(batch_size, device, feature_format) -> Tuple[DataL
     :param feature_format: 2d or flat
     :return: (train, validation, test) data loaders
     """
-    to_tensor_method = ToTensor()
+
     # creates the dataloader for pytorch
-
-    def transform_feature(pil_image):
-        if feature_format == "2d":
-            return to_tensor_method(pil_image).to(device)
-        elif feature_format == "flat":
-            return to_tensor_method(pil_image).reshape(-1).to(device)
-        else:
-            raise ValueError(f"invalid value for feature format:{feature_format}")
-
-    def transform_target(target):
-        return torch.tensor(target).to(device)
-
-    from discrete_nn.settings import dataset_path
-    import os
     mnist_fashion_path = os.path.join(dataset_path, "fashion")
 
-    train_val_dataset = FashionMNIST(mnist_fashion_path, download=True, train=True, transform=transform_feature,
-                                     target_transform=transform_target)
+    feature_fn = partial(_transform_feature, feature_format=feature_format, device=device)
+    target_fn = partial(_transform_target, device=device)
+
+    train_val_dataset = FashionMNIST(mnist_fashion_path, download=True, train=True, transform=feature_fn,
+                                     target_transform=target_fn)
 
     train_size = int(len(train_val_dataset) * 0.8)
     eval_size = len(train_val_dataset) - train_size
@@ -41,7 +33,25 @@ def fashion_mnist_dataloaders(batch_size, device, feature_format) -> Tuple[DataL
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(validation_dataset, batch_size=batch_size)
 
-    test_loader = DataLoader(FashionMNIST(mnist_fashion_path, download=True, train=False, transform=transform_feature,
-                                          target_transform=transform_target),
+    test_loader = DataLoader(FashionMNIST(mnist_fashion_path, download=True, train=False, transform=feature_fn,
+                                          target_transform=target_fn),
                              batch_size=batch_size)
     return train_loader, validation_loader, test_loader
+
+
+# these methods have to stay here bc they need to be serializable by torch
+
+def _transform_target(target, device):
+    return torch.tensor(target).to(device)
+
+
+_to_tensor_method = ToTensor()
+
+
+def _transform_feature(pil_image, feature_format, device):
+    if feature_format == "2d":
+        return _to_tensor_method(pil_image).to(device)
+    elif feature_format == "flat":
+        return _to_tensor_method(pil_image).reshape(-1).to(device)
+    else:
+        raise ValueError(f"invalid value for feature format:{feature_format}")
