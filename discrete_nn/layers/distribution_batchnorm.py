4e7f518aeba8bf431c3d13994e1f67bd19026c7a
thiagobell <thibef@gmail.com>
2020-04-18 16:37:12 +0200

    added support for REAL values in our custom batchnorm implementation

--- discrete_nn/layers/distribution_batchnorm.py
+++ discrete_nn/layers/distribution_batchnorm.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from torch.nn import Module
 import torch
 from discrete_nn.layers.weight_utils import discretize_weights_probabilistic, get_gaussian_dist_parameters
-from discrete_nn.layers.type_defs import InputFormat, DiscreteWeights
+from discrete_nn.layers.type_defs import InputFormat, ValueTypes
 
 
 class DistributionBatchnorm(Module):
@@ -10,10 +11,13 @@ class DistributionBatchnorm(Module):
     Only supports distributions as inputs. Its weights and bias are real
     """
 
-    def __init__(self, input_format: InputFormat, num_features, initialization_weights, initialization_bias):
+    def __init__(self, input_format: InputFormat, value_type: ValueTypes, num_features,
+                 initialization_weights: Optional[torch.tensor],
+                 initialization_bias: Optional[torch.tensor]):
         """
         Initialized a batch normalization layer
         :param input_format: defines if input is a feature map or an array, both of which defined with distributions
+        :param value_type: defines the type of input AND output value
         :param num_features: the number of features in input. In the case of a feature map, corresponds to the number
         :param initialization_weights: the weight with which the layer should be initialized with
         :param initialization_weights: the bias with which the layer should be initialized with
@@ -23,6 +27,12 @@ class DistributionBatchnorm(Module):
         super().__init__()
         self._input_format = input_format
         self._num_feat = num_features
+        self._value_type = value_type
+        if initialization_bias is None:
+            initialization_bias = torch.zeros(num_features)
+        if initialization_weights is None:
+            initialization_weights = torch.ones(num_features)
+
         self.gamma = torch.nn.Parameter(initialization_weights, requires_grad=True)
         self.beta = torch.nn.Parameter(initialization_bias, requires_grad=True)
 
@@ -30,25 +40,46 @@ class DistributionBatchnorm(Module):
         batch_size = x.shape[0]
         if self._input_format == InputFormat.FEATURE_MAP:
             # each channel is a feature
-            # x its a distribution feature map of the shape (batch size x 2 x channels x h x w)
-            # to avoid having gamma be multiplied over the wrong dimension in case one of the dimensions has the same
+            # to avoid having gamma being multiplied over the wrong dimension in case one of the dimensions has the same
             # size as the features (or a similar issue when we subtract the batch means) we move the feature dimension
             # to the innermost position for multiplication when dealing with feature maps.
-            shifted_x = x.transpose(2, 3).transpose(3, 4)
-            # shifted x is of the shape (batch_size x 2 x h x w x channels)
+            if self._value_type == ValueTypes.GAUSSIAN:
+                # x its a distribution feature map of the shape (batch size x 2 x channels x h x w)
+                shifted_x = x.transpose(2, 3).transpose(3, 4)
+                # shifted x is of the shape (batch_size x 2 x h x w x channels)
+                input_mean = shifted_x[:, 0, :, :, :]  # (batch size  x h x w x channels)
+                input_var = shifted_x[:, 1, :, :, :]
+                h = shifted_x.shape[2]
+                w = shifted_x.shape[3]
 
-            input_mean = shifted_x[:, 0, :, :, :]  # (batch size  x h x w x channels)
-            input_var = shifted_x[:, 1, :, :, :]
-            h = shifted_x.shape[2]
-            w = shifted_x.shape[3]
+            elif self._value_type == ValueTypes.REAL:
+                # x its a distribution feature map of the shape (batch size x channels x h x w)
+                shifted_x = x.transpose(1, 2).transpose(2, 3)
+                # shifted x is of the shape (batch_size x h x w x channels)
+                input_mean = shifted_x[:, :, :, :]  # (batch size  x h x w x channels)
+                input_var = 0.0
+                h = shifted_x.shape[1]
+                w = shifted_x.shape[2]
+            else:
+                raise TypeError(f"invalid ValueType:{self._value_type}")
+
             # a vector of size #channels
             batch_mean = torch.sum(input_mean, dim=[0, 1, 2]) / (batch_size * w * h)
             batch_var = torch.sum(input_var + torch.pow(input_mean - batch_mean, 2), dim=[0, 1, 2]) / (
-                    batch_size*h*w - 1)
+                                  batch_size*h*w - 1)
+
         elif self._input_format == InputFormat.FLAT_ARRAY:
-            # its a flat input with distributions
-            input_mean = x[:, 0, :]  # a (batch_size, num_inputs) tensor
-            input_var = x[:, 1, :]
+
+            if self._value_type == ValueTypes.GAUSSIAN:
+                # its a flat input with distributions
+                input_mean = x[:, 0, :]  # a (batch_size, num_inputs) tensor
+                input_var = x[:, 1, :]
+            elif self._value_type == ValueTypes.REAL:
+                # its a flat input with distributions
+                input_mean = x[:, :]  # a (batch_size, num_inputs) tensor
+                input_var = 0.0
+            else:
+                raise TypeError(f"invalid ValueType:{self._value_type}")
             # vector corresponding to number of input features
             batch_mean = torch.sum(input_mean, dim=0) / batch_size
             batch_var = torch.sum(input_var + torch.pow(input_mean - batch_mean, 2), dim=0) / (batch_size - 1)
@@ -56,20 +87,27 @@ class DistributionBatchnorm(Module):
             raise ValueError(f"invalid shape for input:{x.shape}")
 
         assert batch_mean.shape == (self._num_feat,)
-        normalized_mean = (input_mean - batch_mean)/torch.sqrt(batch_var)
-        normalized_var = input_var/batch_var
+        normalized_mean = (input_mean - batch_mean)/torch.sqrt(batch_var + 1e-8)
 
         out_mean = self.gamma * normalized_mean + self.beta
-        out_var = normalized_var * torch.pow(self.gamma, 2)
+        if self._value_type == ValueTypes.GAUSSIAN:
+            normalized_var = input_var/batch_var
+            out_var = normalized_var * torch.pow(self.gamma, 2)
+        else:
+            out_var = None
 
         if self._input_format == InputFormat.FEATURE_MAP:
             # out_mean and out_var are calculated from input mean
-            # which is of the shape (batch size  x h x w x channels)
+            # which is of the shape (batch size x h x w x channels)
             # we need to returns channels dimension to its proper place
             out_mean = out_mean.transpose(3, 2).transpose(2, 1)
-            out_var = out_var.transpose(3, 2).transpose(2, 1)
+            if self._value_type == ValueTypes.GAUSSIAN:
+                out_var = out_var.transpose(3, 2).transpose(2, 1)
 
-        return torch.stack([out_mean, out_var], dim=1)
+        if self._value_type == ValueTypes.GAUSSIAN:
+            return torch.stack([out_mean, out_var], dim=1)
+        else:
+            return out_mean
 
 
 if __name__ == "__main__":
